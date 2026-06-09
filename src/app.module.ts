8c9cbc851c0c4d2bbc261e90bb5c796423fc8c39
Bisikama <minhbao28032005@gmail.com>
2026-06-09 14:17:00 +0700

    add docker database local

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-import { LessonsModule } from './lessons/lessons.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './database/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, LessonsModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
